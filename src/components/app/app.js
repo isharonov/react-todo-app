29b52ed400bd6f26dc528c14bccf313223d4a8c2
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2021-12-13 23:07:09 +0300

    feat: add done, important and remove actions

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
 import ItemStatusFilter from "../item-status-filter";
@@ -6,24 +6,43 @@ import TodoList from "../todo-list";
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
 
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1},
-    { label: 'Make Awesome App', important: true, id: 2},
-    { label: 'Have a lunch', important: false, id: 3}
-  ];
+  state = {
+    todoData: [
+      { label: 'Drink Coffee', important: false, id: 1},
+      { label: 'Make Awesome App', important: true, id: 2},
+      { label: 'Have a lunch', important: false, id: 3}
+    ]
+  };
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3}/>
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  deleteItem = (id) => {
+    this.setState(({todoData}) => {
+      const idx = todoData.findIndex((el) => el.id === id);
+      const newArr = [
+        ...todoData.slice(0, idx),
+        ...todoData.slice(idx + 1)
+      ];
+
+      return {
+        todoData: newArr
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3}/>
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+        <TodoList
+          todos={this.state.todoData}
+          onDeleted={ this.deleteItem }/>
       </div>
-      <TodoList todos={todoData} />
-    </div>
-  );
-};
+    );
+  };
 
-export default App;
+};
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,38 +1,68 @@
-import React from "react";
+import React, { Component } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid, regular } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 
 import './todo-list-item.css'
 
-const TodoListItem = ({ label, important = false }) => {
+export default class TodoListItem extends Component {
 
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
+  state = {
+    done: false,
+    important: false
   };
 
-  return (
-    <div className="todo-list-item">
+  onLabelClick = () => {
+    this.setState(({done}) => {
+      return {
+        done: !done
+      }
+    });
+  };
+
+  onMarkImportant = () => {
+    this.setState(({important}) => {
+      return {
+        important: !important
+      };
+    });
+  };
+
+  render() {
+    const { label, onDeleted } = this.props;
+    const { done, important } = this.state;
+
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
+
+    if (important) {
+      classNames += ' important';
+    }
+
+    return (
+      <div className={classNames}>
       <span
         className="todo-list-item-label"
-        style={style}>
+        onClick={ this.onLabelClick }>
         { label }
       </span>
 
-      <span className="todo-list-item-btns">
+        <span className="todo-list-item-btns">
         <button type="button"
-                className="btn btn-outline-success btn-sm">
+                className="btn btn-outline-success btn-sm"
+                onClick={this.onMarkImportant}>
           <FontAwesomeIcon icon={solid('exclamation')} />
         </button>
 
         <button type="button"
-                className="btn btn-outline-danger btn-sm">
+                className="btn btn-outline-danger btn-sm"
+                onClick={onDeleted}>
           <FontAwesomeIcon icon={regular('trash-alt')} />
         </button>
       </span>
-    </div>
-  );
-};
-
-export default TodoListItem;
+      </div>
+    );
+  }
+}
