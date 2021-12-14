c0a51dc0f8688e95366d14be4914598101a646d7
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2021-12-14 23:16:13 +0300

    feat: add ability to filter items

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,8 @@ export default class App extends Component {
       this.createTodoItem('Make Awesome App'),
       this.createTodoItem('Have a lunch')
     ],
-    term: ''
+    term: '',
+    filter: 'all' // all, active, done
   };
 
   createTodoItem(label) {
@@ -85,6 +86,14 @@ export default class App extends Component {
     });
   };
 
+  onSearchChange = (term) => {
+    this.setState({ term });
+  }
+
+  onFilterChange = (filter) => {
+    this.setState({ filter });
+  }
+
   search(items, term) {
     if (term.length === 0) {
       return items;
@@ -97,15 +106,27 @@ export default class App extends Component {
     })
   }
 
-  onSearchChange = (term) => {
-    this.setState({ term });
+  filter(items, filter) {
+
+    switch(filter) {
+      case 'all':
+        return items;
+      case 'active':
+        return items.filter((item) => !item.done);
+      case 'done':
+        return items.filter((item) => item.done);
+      default:
+        return items;
+    }
   }
 
   render() {
 
-    const { todoData, term } = this.state;
+    const { todoData, term, filter } = this.state;
 
-    const visibleItems = this.search(todoData, term);
+    const visibleItems = this.filter(
+      this.search(todoData, term), filter);
+
     const doneCount = todoData
                       .filter((el) => el.done).length;
     const todoCount = todoData.length - doneCount;
@@ -115,8 +136,12 @@ export default class App extends Component {
         <AppHeader toDo={todoCount} done={doneCount}/>
         <div className="top-panel d-flex">
           <SearchPanel onSearchChange={this.onSearchChange} />
-          <ItemStatusFilter />
+          <ItemStatusFilter
+            filter={filter}
+            onFilterChange={this.onFilterChange}
+          />
         </div>
+
         <TodoList
           todos={visibleItems}
           onDeleted={ this.deleteItem }
